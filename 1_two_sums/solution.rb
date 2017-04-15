001c59153940590c06ccb7f609589658b5219543
Anatoli Makarevich <makaroni4@gmail.com>
2017-04-15 02:23:27 +0200

    Initial commit

--- 1_two_sums/solution.rb
+++ 1_two_sums/solution.rb
@@ -2,11 +2,7 @@ require "test/unit"
 
 def two_sum(nums, target)
   nums.each_index.select do |i|
-    # puts "--> i: #{i}"
-    puts "--> i: #{nums[i]}"
     nums[(i+1)..-1].each_index.select do |j|
-      # puts "--> j: #{i + j + 1}"
-      puts "--> j: #{nums[i + j + 1]}"
       if nums[i] + nums[i + j + 1] == target
         return [i, i + j + 1]
       end
@@ -14,6 +10,5 @@ def two_sum(nums, target)
   end
 end
 
-# puts "COORECT" if two_sum([2, 7, 11, 15], 9) == [0, 1]
-# p two_sum([3, 2, 4], 6)
-puts "COORECT" if two_sum([3, 2, 4], 6) == [1, 2]
+puts "INCOORECT" unless two_sum([2, 7, 11, 15], 9) == [0, 1]
+puts "INCOORECT" unless two_sum([3, 2, 4], 6) == [1, 2]
