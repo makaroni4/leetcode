c11abba551eb6c524a0259621c724ccaaa0a6da4
Anatoli Makarevich <makaroni4@gmail.com>
2017-05-03 20:42:53 +0200

    Added slow solution for 139 word break

---
+++ 139_word_break/solution.rb
@@ -0,0 +1,27 @@
+# @param {String} s
+# @param {String[]} word_dict
+# @return {Boolean}
+def word_break(s, word_dict)
+  set = Hash[word_dict.zip(Array.new(word_dict.size, true))]
+
+  queue = s.size.times.map do |i|
+    [s[0..i], s[i + 1..-1]]
+  end
+
+  while queue.any? do
+    prefix, suffix = queue.pop
+
+    if set.has_key?(prefix)
+      return true if suffix.empty?
+
+      suffix.size.times do |i|
+        queue.push([suffix[0..i], suffix[i + 1..-1]])
+      end
+    end
+  end
+
+  return false
+end
+
+puts "INCORRECT" unless word_break("leetcode", ["leet", "code"])
+puts "INCORRECT" if word_break("leetcode", ["leet", "cod"])
